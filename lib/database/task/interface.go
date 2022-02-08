753a7f00b7bfb237938471b77cdafe0c2630ee00
faliqadlan <faliq_adlan@student.ub.ac.id>
2022-02-08 17:34:53 +0700

    done task controller

--- lib/database/task/interface.go
+++ lib/database/task/interface.go
@@ -4,12 +4,14 @@ import (
 	"part3/models/task"
 	"part3/models/task/request"
 	"part3/models/task/response"
+
+	"gorm.io/gorm"
 )
 
 type Task interface {
 	Create(user_id int, newTask task.Task) (task.Task, error)
 	// GetById(id int, user_id int) (task.Task, error)
 	UpdateById(id int, user_id int, taskReg request.TaskRequest) (task.Task, error)
-	// DeleteById(id int, user_id int) (gorm.DeletedAt, error)
+	DeleteById(id int, user_id int) (gorm.DeletedAt, error)
 	GetAll(user_id int) ([]response.TaskResponse, error)
 }
