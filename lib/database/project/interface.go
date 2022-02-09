d956c94012099316e7b483c02e0b06651a6b518f
faliqadlan <faliq_adlan@student.ub.ac.id>
2022-02-09 12:55:06 +0700

    wait

--- lib/database/project/interface.go
+++ lib/database/project/interface.go
@@ -2,16 +2,12 @@ package project
 
 import (
 	"part3/models/project"
-	"part3/models/project/request"
-	"part3/models/project/response"
-
-	"gorm.io/gorm"
 )
 
 type Project interface {
 	Create(user_id int, newPro project.Project) (project.Project, error)
 	// GetById(id int, user_id int) (project.Project, error)
-	UpdateById(id int, user_id int, upPro request.ProRequest) (project.Project, error)
-	DeleteById(id int, user_id int) (gorm.DeletedAt, error)
-	GetAll(user_id int) ([]response.ProResponse, error)
+	// UpdateById(id int, user_id int, upPro request.ProRequest) (project.Project, error)
+	// DeleteById(id int, user_id int) (gorm.DeletedAt, error)
+	// GetAll(user_id int) ([]response.ProResponse, error)
 }
