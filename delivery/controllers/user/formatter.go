30b0a7ec39e4232aad2473b3c375bdbb52fb7143
faliqadlan <faliq_adlan@student.ub.ac.id>
2022-02-10 21:33:35 +0700

    done check user cont

--- delivery/controllers/user/formatter.go
+++ delivery/controllers/user/formatter.go
@@ -2,12 +2,6 @@ package user
 
 import "part3/models/user"
 
-type UserRegister struct {
-	Name     string `json:"name"`
-	Email    string `json:"email" `
-	Password string `json:"password"`
-}
-
 type GetUserResponseFormat struct {
 	Code    int       `json:"code"`
 	Message string    `json:"message"`
